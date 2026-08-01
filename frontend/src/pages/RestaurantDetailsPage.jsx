6c532ccb09d4731e05e6f6b831b8309bc36ef360
Tang Jasmine <jasminet2301@gmail.com>
2026-08-01 18:01:29 +0800

    fix vite error

--- frontend/src/pages/RestaurantDetailsPage.jsx
+++ frontend/src/pages/RestaurantDetailsPage.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useState} from "react";
-import {Link, useParams} from "react-router-dom";
+import {Link, useParams} from "react-router";
 import {getRestaurant} from "../api/restaurants";
 
 function RestaurantDetailsPage() {
